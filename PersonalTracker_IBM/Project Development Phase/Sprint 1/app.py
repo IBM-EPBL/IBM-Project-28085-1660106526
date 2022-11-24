0800fb46563fb2cf8e1896950d77dffce92b1f51
neeraja2001 <88621434+neeraja2001@users.noreply.github.com>
2022-11-24 13:56:03 +0530

    Add files via upload

--- PersonalTracker_IBM/Project Development Phase/Sprint 1/app.py
+++ PersonalTracker_IBM/Project Development Phase/Sprint 1/app.py
@@ -1,79 +1,50 @@
-import os
-import pathlib
-import requests
-from flask import Flask, session, abort, redirect, request
-from flask.templating import render_template
-from google.oauth2 import id_token
-from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
-import google.auth.transport.requests
-app = Flask("Google Login App")  #naming our application
-app.secret_key = "GeekyHuman.com"  #it is necessary to set a password when dealing with OAuth 2.0
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  #this is to set our environment to https because OAuth 2.0 only supports https environments
+from flask import Flask, request, redirect
+from flask import Blueprint,render_template
+app = Flask(__name__)
 
-GOOGLE_CLIENT_ID = "<your_client_id>"  #enter your client id you got from Google console
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")  #set the path to where the .json file you got Google console is
+app.debug = True
 
-flow = Flow.from_client_secrets_file(  #Flow is OAuth 2.0 a class that stores all the information on how we want to authorize our users
-    client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],  #here we are specifing what do we get after the authorization
-    redirect_uri="http://127.0.0.1:5000/callback"  #and the redirect URI is the point where the user will end up after the authorization
-)
-def login_is_required(function):  #a function to check if the user is authorized or not
-    def wrapper(*args, **kwargs):
-        if "google_id" not in session:  #authorization required
-            return abort(401)
-        else:
-            return function()
+@app.route('/')
+def landingPage():
+    return render_template("landingPage.html")
 
-    return wrapper
+@app.route('/navbar')
+def navbar():
+    return render_template("Navbar.html")
 
 
-@app.route("/login")  #the page where the user can login
-def login():
-    authorization_url, state = flow.authorization_url()  #asking the flow class for the authorization (login) url
-    session["state"] = state
-    return redirect(authorization_url)
+@app.route('/contactus')
+def contactus():
+    return render_template("contactUs.html")
 
+@app.route('/signup')
+def signup():
+    return render_template("signUp.html")
 
-@app.route("/callback")  #this is the page that will handle the callback process meaning process after the authorization
-def callback():
-    flow.fetch_token(authorization_response=request.url)
+@app.route('/signin')
+def signin():
+    return render_template("signIn.html")
 
-    if not session["state"] == request.args["state"]:
-        abort(500)  #state does not match!
+@app.route('/aboutus')
+def aboutus():
+    return render_template("aboutUs.html")
 
-    credentials = flow.credentials
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+@app.route('/skeleton')
+def skeleton():
+    return render_template("skeleton.html")
 
-    id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token,
-        request=token_request,
-        audience=GOOGLE_CLIENT_ID
-    )
-
-    session["google_id"] = id_info.get("sub")  #defing the results to show on the page
-    session["name"] = id_info.get("name")
-    return redirect("/dashboard")  #the final page where the authorized users will end up
-
-
-@app.route("/logout")  #the logout page and function
-def logout():
-    session.clear()
-    return redirect("/")
-
-
-@app.route("/")  #the home page where the login button will be located
-def signUp():
-     return render_template('signUp.html')
-
-@app.route("/")  #the page where only the authorized users can go to
-@login_is_required
+@app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template("dashboard.html")
+
+@app.route('/addingExpenses')
+def addingExpenses():
+    return render_template("addingExpenses.html")
+    
+    
+if __name__ == '__main__':
+    app.run()
 
 
-if __name__ == "__main__":  #and the final closing function
-    app.run(debug=True)
+
+
